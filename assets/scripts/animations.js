31d096b9fa92aa8cf97dd1b3da66f80883841eac
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-01-08 21:02:55 +0200

    gsap: scrollsmoother active

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -1,20 +1,6 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-// 1. Grab all elements with your 'scroll' class
-const scrollElements = document.querySelectorAll(".sroll-trigger");
-
-// 2. Loop through each one individually
-scrollElements.forEach((el) => {
-  gsap.from(el, {
-    scrollTrigger: {
-      trigger: el,      // The trigger is the element ITSELF
-      start: "top 85%", // Start when the element is near the bottom
-      end: "top 50%",
-      scrub: 1,         // Smooth catch-up
-      markers: false    // Turn on to debug
-    },
-    y: 50,
-    opacity: 0,
-    duration: 1
-  });
-});
+let smoother = ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+})
